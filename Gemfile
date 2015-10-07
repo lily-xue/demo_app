de72b83cc6dad4e423972efa910b8870ad9cac90
lily-xue <414564926@qq.com>
2015-10-07 08:05:48 -0700

    init new demoapp

--- Gemfile
+++ Gemfile
@@ -5,8 +5,14 @@ ruby '2.1.7'
 gem 'rails', '4.0.2'
 
 # Use sqlite3 as the database for Active Record
-group :development do 
+group :development,:test do 
 gem 'sqlite3','1.3.8'
+gem 'rspec-rails','2.13.1'
+end
+
+group :test do
+gem 'selenium-webdriver','2.35.1'
+gem 'capybara','2.1.0'
 end
 
 # Use SCSS for stylesheets
@@ -25,7 +31,8 @@ gem 'coffee-rails', '4.0.1'
 gem 'jquery-rails','2.2.1'
 
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+gem 'turbolinks','1.1.1'
+
 
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '1.0.2'
@@ -37,7 +44,6 @@ end
 
 group :production do
 gem 'pg','0.15.1'
-gem 'rails_12factor','0.0.2'
 end
 
 # Use ActiveModel has_secure_password
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,23 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-DemoApp::Application.config.secret_key_base = '00b8b824c9ec0887c80f7ed1782690a8ee0001896bb07c0ee2d77b44442d3de65423a84157c4d8d5cb0a68ec07af63397eb336dfd964fa9633110d7fb5097705'
+#DemoApp::Application.config.secret_key_base = '00b8b824c9ec0887c80f7ed1782690a8ee0001896bb07c0ee2d77b44442d3de65423a84157c4d8d5cb0a68ec07af63397eb336dfd964fa9633110d7fb5097705'
+
+
+#require 'securerandom'
+
+#def secure_token
+#token_file = Rails.root.join('.secret')
+#if File.exist?(token_file)
+#File.read(token_file).chomp
+#else
+#token = SecreRandom.hex(64)
+#File.write(token_file,token)
+#token
+#end
+#end
+#DemoApp::Application.config.secret_key_base = secure_token
+
+
+ 
+#DemoApp::Application.config.secret_key_base = '00b8b824c9ec0887c80f7ed1782690a8ee0001896bb07c0ee2d77b44442d3de65423a84157c4d8d5cb0a68ec07af63397eb336dfd964fa9633110d7fb5097705'
